4912895b7f5e179fe525dc237a0f48fd251dc6a4
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-07-06 15:31:21 -0700

    linter fixes

--- frontend/src/components/Paint.vue
+++ frontend/src/components/Paint.vue
@@ -3,58 +3,56 @@
     <h2>{{ msg }}</h2>
     <h3>Draw this ^^</h3>
     <div id="painterro" class="paint"></div>
-
   </div>
 </template>
 
 <script>
-import Painterro from 'painterro';
+import Painterro from 'painterro'
 
 export default {
   name: 'Paint',
-  data() {
+  data () {
     return {
       painterro: null,
-      msg: 'A cat winks at you with the grace of a very sleepy toddler.',
+      msg: 'A cat winks at you with the grace of a very sleepy toddler.'
     }
   },
-  mounted() {
-      this.painterro = Painterro({
-        id: 'painterro',
-        colorScheme: {
-          main: '#f8f8f8',
-          control: '#d5d5d5',
-          controlContent: '#434649'
-        },
-        hiddenTools: ['crop', 'select', 'text', 'rotate', 'resize', 'open', 'close', 'zoomin', 'zoomout', 'settings', 'pixelize', 'file'],
-        defaultTool: 'brush',
-        defaultLineWidth: 8,
-        activeColor: '#000000',
-        activeColorAlpha: '1',
-        availableEraserWidths: [1, 2, 4, 8, 16, 64],
-        availableLineWidths: [1, 2, 4, 8, 16, 64],
-        availableArrowLengths: [10, 40, 100],
-        saveHandler: (image, done) => {
-          const type = 'image/png';
-          const file = new File([image.asBlob(type)], "file.png", {
-            type: type,
-          });
-          this.add_file(file);
-          done(true); //done and hide painterro
-        }
-      });
-      this.painterro.show();
-    },
+  mounted () {
+    this.painterro = Painterro({
+      id: 'painterro',
+      colorScheme: {
+        main: '#f8f8f8',
+        control: '#d5d5d5',
+        controlContent: '#434649'
+      },
+      hiddenTools: ['crop', 'select', 'text', 'rotate', 'resize', 'open', 'close', 'zoomin', 'zoomout', 'settings', 'pixelize', 'file'],
+      defaultTool: 'brush',
+      defaultLineWidth: 8,
+      activeColor: '#000000',
+      activeColorAlpha: '1',
+      availableEraserWidths: [1, 2, 4, 8, 16, 64],
+      availableLineWidths: [1, 2, 4, 8, 16, 64],
+      availableArrowLengths: [10, 40, 100],
+      saveHandler: (image, done) => {
+        const type = 'image/png'
+        const file = new File([image.asBlob(type)], 'file.png', {
+          type: type
+        })
+        this.add_file(file)
+        done(true)
+      }
+    })
+    this.painterro.show()
+  },
   methods: {
-    add_file(file) {
+    add_file (file) {
       console.log(file)
-    },
+    }
   }
 }
 
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1,
 h2 {
