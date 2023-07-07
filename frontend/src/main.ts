4ce464e325ec6d79d332cc7d62f4e18799a1cfb5
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-07-06 21:24:15 -0700

    added watch

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,22 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, watch } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia();
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
+
+watch(
+    pinia.state,
+    (state) => {
+      localStorage.setItem("auth", JSON.stringify(state.auth));
+    },
+    { deep: true }
+    );
---
+++ frontend/src/stores/auth.ts
@@ -0,0 +1,31 @@
+import { defineStore } from 'pinia'
+
+export const useAuth = defineStore('auth', {
+  state: () => {
+    if (localStorage.getItem('auth')) {
+      return JSON.parse(localStorage.getItem('auth')!!)
+    } else {
+      return {
+        access_token: null,
+        renew_token: null,
+        endpoints: {
+          obtainJWT: 'http://0.0.0.0:8000/api/token/',
+          refreshJWT: 'http://0.0.0.0:8000/api/token/refresh/'
+        }
+      }
+    }
+  },
+  getters: {
+    accessToken: (state) => state.access_token,
+    renewToken: (state) => state.renew_token
+  },
+  actions: {
+    updateAccessToken (newAccessToken: string) {
+      console.log(this)
+      this.access_token = newAccessToken
+    },
+    updateRenewToken (newRenewToken: string) {
+      this.renew_token = newRenewToken
+    }
+  }
+})
