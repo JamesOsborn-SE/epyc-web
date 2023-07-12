7edcee18bc17530624bf641e7214f0a4b827adda
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-07-12 09:20:15 -0700

    get auth working

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { defineStore } from 'pinia'
 
 export const useAuth = defineStore('auth', {
@@ -6,26 +7,70 @@ export const useAuth = defineStore('auth', {
       return JSON.parse(localStorage.getItem('auth')!!)
     } else {
       return {
-        access_token: null,
-        renew_token: null,
+        accessToken: null,
+        refreshToken: null,
+        refreshTokenTimeout: null,
         endpoints: {
-          obtainJWT: 'http://0.0.0.0:8000/api/token/',
-          refreshJWT: 'http://0.0.0.0:8000/api/token/refresh/'
+          obtainJWT: 'http://127.0.0.1:8000/api/token/',
+          refreshJWT: 'http://127.0.0.1:8000/api/token/refresh/'
         }
       }
     }
   },
   getters: {
-    accessToken: (state) => state.access_token,
-    renewToken: (state) => state.renew_token
+    getAccessToken: (state) => state.accessToken,
+    getHeaders(state) {
+      return {
+        headers: {
+          'Authorization': 'Bearer ' + this.accessToken
+        }
+      }
+    }
   },
   actions: {
-    updateAccessToken (newAccessToken: string) {
-      console.log(this)
-      this.access_token = newAccessToken
+    updateAccessToken(newAccessToken: string) {
+      this.accessToken = newAccessToken
     },
-    updateRenewToken (newRenewToken: string) {
-      this.renew_token = newRenewToken
+    updateRenewToken(newRenewToken: string) {
+      this.refreshToken = newRenewToken
+    },
+    async login(username: string, password: string) {
+      axios.post(this.endpoints.obtainJWT,
+        {
+          username: username,
+          password: password
+        }
+      )
+        .then(response => {
+          this.accessToken = response.data.access
+          this.refreshToken = response.data.refresh
+          this.startRefreshTokenTimer()
+        })
+    },
+    async refreshAccessToken() {
+      await axios.post(this.endpoints.refreshJWT, {}, {
+        headers: {
+          'Authorization': 'Bearer ' + this.accessToken
+        }
+      })
+        .then(response => {
+          this.accessToken = response.data.access
+          this.refreshToken = response.data.refresh
+          this.startRefreshTokenTimer();
+        })
+    },
+    startRefreshTokenTimer() {
+      // parse json object from base64 encoded jwt token
+      const jwtBase64 = this.accessToken.split('.')[1];
+      const jwtToken = JSON.parse(atob(jwtBase64));
+
+      // set a timeout to refresh the token a minute before it expires
+      const expires = new Date(jwtToken.exp * 1000);
+      const timeout = expires.getTime() - Date.now() - (60 * 1000);
+      this.refreshTokenTimeout = setTimeout(this.refreshAccessToken, timeout);
+    },
+    stopRefreshTokenTimer() {
+      clearTimeout(this.refreshTokenTimeout);
     }
   }
 })
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useAuth } from '../stores/auth'
+import router from '@/router'
+
+const authStore = useAuth()
+
+const username = ref("")
+const password = ref("")
+const isSubmitting = ref(false)
+const errors = ref([])
+
+// redirect to home if already logged in
+if (authStore.refreshToken) {
+    router.push('/')
+}
+
+function onSubmit() {
+    isSubmitting.value = true
+    return authStore.login(username.value, password.value)
+        .then(() => {
+            if (authStore.accessToken)
+                router.push('/');
+        })
+        .catch()
+        .finally(()=>{
+            isSubmitting.value = false
+        })
+}
+</script>
+
+<template>
+    <div class="col-md-6 offset-md-3 mt-5">
+        
+        <h2>Login</h2>
+        <form :onsubmit="onSubmit">
+
+            <div class="mb-3">
+                <label class="form-label">Username</label>
+                <input v-model="username" :required="true">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Password</label>
+                <input v-model="password" type="password" :required="true" :minlength="3">
+            </div>
+            <div class="mb-3">
+                <button class="btn btn-primary" type="submit" value="submit" :disabled="isSubmitting">
+                    <span v-show="isSubmitting" class="spinner-border spinner-border-sm me-1"></span>
+                    Login
+                </button>
+            </div>
+            <p v-if="errors.values.length">
+                <b>Please correct the following error(s):</b>
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+            </p>
+        </form>
+    </div>
+</template>
