c899de8fbbfa0de6e52ee21267497758aca1d8d2
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-07-13 11:12:17 -0700

    stub in entry view

---
+++ frontend/src/views/EntryView.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts"> 
+    import { useGame } from '@/stores/game';
+    import Paint from '../components/Paint.vue'
+    import { defineProps, onMounted } from "vue"
+    import { useRouter } from "vue-router"
+    import type { Entry } from '@/types/Entry';
+    
+    const gameStore = useGame()
+    const props = defineProps( {
+      id: {
+        type: String,
+        required: true,
+      }
+    })
+    onMounted( () => {
+        console.log(props.id)
+        const e = gameStore.getEntry(props.id)
+        // debugger
+        e.then( (r: Entry) =>{
+          console.log(r)
+        })
+    } )
+
+</script>
+
+
+<template>
+  <div class="about">
+    <h1>{{ $route.params.id }}</h1>
+    <div v-if="$">
+
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
