046563947005e61cdac000748b9f172892864e33
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-07-13 17:25:54 -0700

    added get latest entry

--- frontend/src/stores/game.ts
+++ frontend/src/stores/game.ts
@@ -16,29 +16,41 @@ export const useGame = defineStore('game', {
     }
   },
   getters: {
-    getGame: (state: Game) => state
+    getGame: (state) => state,
+    getLatestEntry(state) {
+      if(state.entries && state.entries.length > 0){
+        return state.entries.sort((a: Entry, b: Entry) => a.sequence > b.sequence).slice(0,1)[0]
+      } else {
+        return null
+      }
+    } 
   },
   actions: {
     async newGame(): Promise<Game> {
       const authStore = useAuth()
       return axios.post('http://127.0.0.1:8000/api/games/', {}, authStore.getHeaders)
         .then(response => {
+          this.id = response.data.id
           return response.data as Game
-        })
+        })// todo: add catches for 401,etc
     },
     async getEntry(id: string): Promise<Entry> {
       const authStore = useAuth()
       return axios.get('http://127.0.0.1:8000/api/entries/' + id, authStore.getHeaders)
         .then(response => {
+          this.id = response.data.game_id
+          this.entries = [ response.data as Entry ]
           return response.data as Entry
-        })
+        })// todo: add catches for 401,etc
     },
     async setEntry(entry: Entry): Promise<Array<Entry>> {
       const authStore = useAuth()
       return axios.post('http://127.0.0.1:8000/api/entries/', entry, authStore.getHeaders)
         .then(response => {
+          this.id = response.data.game_id
+          this.entries = response.data as Entry
           return response.data as Array<Entry>
-        })
+        })// todo: add catches for 401,etc
     }
   }
 })
