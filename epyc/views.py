548195dc8ce663a9f7fcf4f1d2c0b35176529c1f
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-07-19 13:40:54 -0700

    add entry on new game

--- epyc/views.py
+++ epyc/views.py
@@ -33,12 +33,29 @@ class GameListApiView(APIView):
         data = {
              'user': request.user.id
         }
-        serializer = GameSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        game_serializer = GameSerializer(data=data)
+        if game_serializer.is_valid():
+            game_serializer.save()
+            entry_data = {
+                'user': request.user.id,
+                'sentence': request.data.get('sentence'),
+                'sequence': 0,
+                'drawing': None,
+                'game_id': game_serializer.data["id"],
+            }
+            entry_serializer = EntrySerializer(data=entry_data)
+            if entry_serializer.is_valid():
+                entry_serializer.save()
+                game_serializer.data["entries"] = [ entry_serializer.data ]
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({
+                'id': game_serializer.data["id"],
+                'created_at': game_serializer.data["created_at"],
+                'user': game_serializer.data["user"],
+                'entries': [entry_serializer.data]
+                }, status=status.HTTP_201_CREATED)
+
+        return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class EntryListApiView(APIView):
     permission_classes = [permissions.IsAuthenticated]
