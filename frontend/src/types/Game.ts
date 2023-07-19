c190258c7f7cd6c66d604a7d72d94e940b79c953
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-07-19 13:42:24 -0700

    added missing fields for game

--- frontend/src/types/Game.ts
+++ frontend/src/types/Game.ts
@@ -1,5 +1,10 @@
 import { Entry } from "./Entry";
 
 export class Game{
-    constructor(public id: string | null, public entries: Array<Entry>){}
+    constructor(
+        public id: string | null,
+        public created_at: Date,
+        public user: string,
+        public entries: Array<Entry>        
+        ){}
 }
