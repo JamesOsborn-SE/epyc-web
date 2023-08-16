6a44d5ff8bb55eb3aac4d04f422f9118791df891
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-08-16 11:16:02 -0700

    add completed at to ts model

--- frontend/src/types/Game.ts
+++ frontend/src/types/Game.ts
@@ -4,6 +4,7 @@ export class Game{
     constructor(
         public id: string | null,
         public created_at: Date,
+        public completed_at: Date,
         public user: string,
         public entries: Array<Entry>
         ){}
