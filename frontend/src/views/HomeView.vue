d5c34fab71f3a7124164e9471d3bf7e67a71aad9
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-08-30 14:56:43 -0700

    add branding

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,6 +5,7 @@
   <main>
   <br>
   <br>
+  <img alt="eat poop you cat being played on a scroll of paper" src="/images/epyc-feature.png" style="width: 100%;" />
     <p>
       Eat Poop You Cat is a bit like telephone meets that one game where you try to get folks to guess the drawing.
     </p>
@@ -14,5 +15,7 @@
       Then they pass it to the next person and they can only see the last entry (picture in this case) and they write a sentence.
       It goes on until everyone has had a turn or boredom takes hold.
     </p>
+    <br>
+    <a href="https://github.com/JamesOsborn-SE/epyc-web">Git the Source</a>
   </main>
 </template>
