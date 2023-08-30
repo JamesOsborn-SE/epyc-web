364e387797aadcc4a4ee299b005ecc3b863f5e21
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-08-30 11:25:26 -0700

    fix ts compiler errors in IDE

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -8,7 +8,7 @@ import OldGamesView from '@/views/OldGamesView.vue'
 import EntryView from '@/views/EntryView.vue'
 import AboutView from '@/views/AboutView.vue'
 import LogoutView from '@/views/LogoutView.vue'
-import { useAuth } from '@/stores/auth'
+import { useAuth } from '@/stores/auth.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
