2574d055fe5eed50c9c8df9ef66356cf00ce9bdc
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-08-30 13:55:25 -0700

    minor refactoring

--- frontend/src/stores/game.ts
+++ frontend/src/stores/game.ts
@@ -3,8 +3,18 @@ import { defineStore } from 'pinia'
 import { Entry } from "@/types/Entry";
 import { Game } from "@/types/Game";
 import { useAuth } from '@/stores/auth'
+import router from '@/router';
 
 const backendHostname = import.meta.env.VITE_BACKEND_HOSTNAME
+
+const handleError = (err: AxiosError) => {
+  if (err.response?.status == 401) {
+    router.push({name:"login"})
+    return;
+  } else {
+    console.log(err)
+  }
+}
 
 export const useGame = defineStore('game', {
   state: () => {
@@ -31,86 +41,72 @@ export const useGame = defineStore('game', {
   },
   actions: {
     async getLastEntry(gameId: string, onlyLastEntryWithImage: boolean) {
-      const withImage = onlyLastEntryWithImage ? "Image" : ""
       const authStore = useAuth()
-      return axios.get(backendHostname + '/api/games/' + gameId + '/entries/last' + withImage, authStore.getHeaders)
+      const withImage = onlyLastEntryWithImage ? "Image" : ""
+      return axios.get(`${backendHostname}/api/games/${gameId}/entries/last${withImage}`, authStore.getHeaders)
         .then(response => {
-          this.id = response.data.id
-          this.created_at = response.data.created_at
-          this.user = response.data.user
+          Object.assign(this, response.data)
           this.entries = [response.data.entries as Entry]
           return response.data as Entry
         })
-        .catch((err: AxiosError) => {
-          if (err.response?.status == 500) {
-            return new Entry(null, null, null, gameId, 99, null, "/images/noimage.png")
-          } else {
-            console.log(err)
-          }
-        })
+        .catch(handleError)
     },
     async getGames() {
       const authStore = useAuth()
-      return axios.get(backendHostname + '/api/games/', authStore.getHeaders)
+      return axios.get(`${backendHostname}/api/games/`, authStore.getHeaders)
         .then(response => {
-          this.id = response.data.id
-          this.created_at = response.data.created_at
-          this.user = response.data.user
+          Object.assign(this, response.data)
           this.entries = response.data.entries as Array<Entry>
           const games = response.data as Game[]
-          games.sort((a: Game, b: Game) => {
-            if (a.created_at > b.completed_at)
-              return -1
-            else
-              return 1
-          })
+          games.sort((a: Game, b: Game) => a.created_at > b.completed_at ? -1 : 1)
           return games;
-        })// todo: add catches for 401,etc
+        })
+        .catch(handleError)
     },
-    async newGame(sentence: string): Promise<Game> {
+    async newGame(sentence: string): Promise<Game | void> {
       const authStore = useAuth()
-      return axios.post(backendHostname + '/api/games/', {
-        sentence: sentence
-      }, authStore.getHeaders)
+      return axios.post(`${backendHostname}/api/games/`, { sentence }, authStore.getHeaders)
         .then(response => {
-          this.id = response.data.id
-          this.created_at = response.data.created_at
-          this.user = response.data.user
+          Object.assign(this, response.data)
           this.entries = response.data.entries as Array<Entry>
           return response.data as Game
-        })// todo: add catches for 401,etc
+        })
+        .catch(handleError)
     },
-    async getEntries(gameId: string): Promise<Entry[]> {
+    async getEntries(gameId: string): Promise<Entry[] | void> {
       const authStore = useAuth()
-      return axios.get(backendHostname + '/api/games/' + gameId + '/entries', authStore.getHeaders)
+      return axios.get(`${backendHostname}/api/games/${gameId}/entries`, authStore.getHeaders)
         .then(response => {
           this.id = response.data.id
           this.entries = response.data as Entry[]
           return this.entries
-        })// todo: add catches for 401,etc
+        })
+        .catch(handleError)
     },
-    async getEntry(id: string): Promise<Entry> {
+    async getEntry(id: string): Promise<Entry | void> {
       const authStore = useAuth()
-      return axios.get(backendHostname + '/api/entries/' + id, authStore.getHeaders)
+      return axios.get(`${backendHostname}/api/entries/${id}`, authStore.getHeaders)
         .then(response => {
           this.id = response.data.game_id
           this.entries = [response.data as Entry]
           return response.data as Entry
-        })// todo: add catches for 401,etc
+        })
+        .catch(handleError)
     },
-    async newEntry(entry: Entry): Promise<Array<Entry>> {
+    async newEntry(entry: Entry): Promise<Array<Entry> | void> {
       const authStore = useAuth()
-      return axios.post(backendHostname + '/api/entries/', entry, authStore.getHeaders)
+      return axios.post(`${backendHostname}/api/entries/`, entry, authStore.getHeaders)
         .then(response => {
           this.id = response.data.game_id
           this.entries = [response.data as Entry]
           return response.data as Array<Entry>
-        })// todo: add catches for 401,etc
+        })
+        .catch(handleError)
     },
     async endGame(gameId: string) {
       const authStore = useAuth()
-      return axios.post(backendHostname + '/api/games/' + gameId + '/end', {}, authStore.getHeaders)
-      // todo: add catches for 401,etc
+      return axios.post(`${backendHostname}/api/games/${gameId}/end`, {}, authStore.getHeaders)
+        .catch(handleError)
     }
   }
 })
