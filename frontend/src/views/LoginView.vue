8670e5f394f6632e2f8b0325dbfdf3f13c22f96b
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-09-01 10:17:57 -0700

    use loading spinner

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from 'vue'
 import { useAuth } from '../stores/auth'
 import router from '@/router'
 import { useRoute } from 'vue-router';
+import PencilSpinner from '@/components/PencilSpinner.vue';
 
 const authStore = useAuth()
 
@@ -13,7 +14,7 @@ const errors = ref([])
 
 const route = useRoute()
 const code = route.query["code"]
-
+const isLoading = ref(true)
 
 // redirect to home if already logged in
 if (!code && !authStore.hasExpired) {
@@ -36,44 +37,43 @@ async function onSubmit(event: Event) {
 }
 onMounted(() => {
     if (code) {
+        new Promise(resolve => setTimeout(resolve, 5000));
         authStore.getAuthFromCode(code).then((path: string) => {
             router.push(path)
+        }).finally(() => {
+            isLoading.value = false
         })
+    } else {
+        isLoading.value = false
     }
 })
 </script>
 
 <template>
-    <Suspense>
-        <template #default>
-            <div class="col-md-6 offset-md-3 mt-5">
-
-                <h2>Login</h2>
-                <form id="loginForm" @submit="onSubmit">
-
-                    <div class="mb-3">
-                        <label class="form-label">Username</label>
-                        <input v-model="username" :required="true">
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Password</label>
-                        <input v-model="password" type="password" :required="true" :minlength="3">
-                    </div>
-                    <div class="mb-3">
-                        <button class="btn btn-primary" type="submit" value="submit" :disabled="isSubmitting">
-                            <span v-show="isSubmitting" class="spinner-border spinner-border-sm me-1"></span>
-                            Login
-                        </button>
-                    </div>
-                    <p v-if="errors.values.length">
-                        <b>Please correct the following error(s):</b>
-                    <ul>
-                        <li v-for="error in errors">{{ error }}</li>
-                    </ul>
-                    </p>
-                </form>
+    <div class="col-md-6 offset-md-3 mt-5">
+        <PencilSpinner v-if="isLoading" />
+        <h2>Login</h2>
+        <form id="loginForm" @submit="onSubmit">
+            <div class="mb-3">
+                <label class="form-label">Username</label>
+                <input v-model="username" :required="true">
             </div>
-        </template>
-        <template #fallback> Loading... </template>
-    </Suspense>
+            <div class="mb-3">
+                <label class="form-label">Password</label>
+                <input v-model="password" type="password" :required="true" :minlength="3">
+            </div>
+            <div class="mb-3">
+                <button class="btn btn-primary" type="submit" value="submit" :disabled="isSubmitting">
+                    <span v-show="isSubmitting" class="spinner-border spinner-border-sm me-1"></span>
+                    Login
+                </button>
+            </div>
+            <p v-if="errors.values.length">
+                <b>Please correct the following error(s):</b>
+            <ul>
+                <li v-for="error in errors">{{ error }}</li>
+            </ul>
+            </p>
+        </form>
+    </div>
 </template>
