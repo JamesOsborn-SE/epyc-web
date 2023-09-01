21661164125f0f6a2567613c6de0109c770fc0a1
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-09-01 10:18:17 -0700

    more toast

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,10 +6,14 @@ import { createPinia } from 'pinia'
 import App from '@/App.vue'
 import router from './router'
 
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
 const app = createApp(App)
 const pinia = createPinia();
 app.use(pinia)
 app.use(router)
+app.use(Toast)
 
 router.isReady().then(() => {
   app.mount("#app");
