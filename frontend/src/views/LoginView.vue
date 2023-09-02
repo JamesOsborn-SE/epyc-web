125613703c9fb0491b8d36ea28dd05e9ac197097
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-09-02 14:40:23 -0700

    revert to refs to get tests working.

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, reactive } from 'vue'
+import { mergeProps, onMounted, reactive, ref } from 'vue'
 import { useAuth } from '../stores/auth'
 import router from '@/router'
 import { useRoute } from 'vue-router';
@@ -7,12 +7,11 @@ import PencilSpinner from '@/components/PencilSpinner.vue';
 
 const authStore = useAuth()
 
-const state = reactive({
-    username: "",
-    password: "",
-    isSubmitting: false,
-    isLoading: true,
-})
+
+const username = ref("")
+const password = ref("")
+const isSubmitting = ref(false)
+const isLoading = ref(true)
 
 const route = useRoute()
 const code = route.query["code"]
@@ -25,15 +24,15 @@ if (!code && !authStore.hasExpired) {
 async function onSubmit(event: Event) {
     event.preventDefault()
 
-    state.isSubmitting = true
-    await authStore.login(state.username, state.password)
+    isSubmitting.value = true
+    await authStore.login(username.value, password.value)
         .then(() => {
             if (!authStore.hasExpired)
                 router.push('/');
         })
         .catch()
         .finally(() => {
-            state.isSubmitting = false
+            isSubmitting.value = false
         })
 }
 onMounted(() => {
@@ -42,30 +41,30 @@ onMounted(() => {
         authStore.getAuthFromCode(code).then((path: string) => {
             router.push(path)
         }).finally(() => {
-            state.isLoading = false
+            isLoading.value = false
         })
     } else {
-        state.isLoading = false
+        isLoading.value = false
     }
 })
 </script>
 
 <template>
     <div class="col-md-6 offset-md-3 mt-5">
-        <PencilSpinner :show="state.isLoading"/>
+        <PencilSpinner :show="isLoading"/>
         <h2>Login</h2>
         <form id="loginForm" @submit="onSubmit">
             <div class="mb-3">
                 <label class="form-label">Username</label>
-                <input v-model="state.username" :required="true">
+                <input v-model="username" :required="true">
             </div>
             <div class="mb-3">
                 <label class="form-label">Password</label>
-                <input v-model="state.password" type="password" :required="true" :minlength="3">
+                <input v-model="password" type="password" :required="true" :minlength="3">
             </div>
             <div class="mb-3">
-                <button class="btn btn-primary" type="submit" value="submit" :disabled="state.isSubmitting">
-                    <span v-show="state.isSubmitting" class="spinner-border spinner-border-sm me-1"></span>
+                <button class="btn btn-primary" type="submit" value="submit" :disabled="isSubmitting">
+                    <span v-show="isSubmitting" class="spinner-border spinner-border-sm me-1"></span>
                     Login
                 </button>
             </div>
---
+++ frontend/src/views/__tests__/LoginView.spec.ts
@@ -0,0 +1,58 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { createTestingPinia } from '@pinia/testing'
+import { mount } from '@vue/test-utils'
+import LoginView from '@/views/LoginView.vue'
+import { useAuth } from '@/stores/auth'
+import { useRoute, } from 'vue-router'
+import { nextTick } from 'vue'
+
+vi.mock('vue-router', () => ({
+  useRoute: vi.fn().mockReturnValue({
+    query: { code: '123' },
+  }),
+  useRouter: vi.fn().mockReturnValue({
+    push: vi.fn(),
+    beforeEach: vi.fn(),
+    afterEach: vi.fn(),
+  }),
+  createRouter: vi.fn().mockReturnValue({
+    push: vi.fn(),
+    beforeEach: vi.fn(),
+  }),
+  createWebHistory: vi.fn(),
+}));
+
+vi.mock('@/stores/auth', () =>({
+  useAuth: vi.fn().mockReturnValue({
+    getAuthFromCode: vi.fn().mockResolvedValue("")
+  })
+}))
+
+describe('LoginView.vue', () => {
+
+
+  let wrapper: any
+  let authStub: any
+  let routeStub: any
+  beforeEach(() => {
+
+  })
+
+  it('renders login form', () => {
+
+
+    authStub = useAuth()
+    authStub.hasExpired = true
+    routeStub = useRoute()
+
+    wrapper = mount(LoginView, {
+      global: {
+        plugins: [createTestingPinia()],
+      }
+    })
+
+    const test = wrapper.vm
+    expect(wrapper.find('#loginForm').exists()).toBe(true)
+  })
+
+})
