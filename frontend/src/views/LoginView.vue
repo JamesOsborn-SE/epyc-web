ea68051ef81c4bc4b4d8bf6090b927341a626ae5
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-09-02 04:15:59 -0700

    refactor to use reactive

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, reactive } from 'vue'
 import { useAuth } from '../stores/auth'
 import router from '@/router'
 import { useRoute } from 'vue-router';
@@ -7,14 +7,15 @@ import PencilSpinner from '@/components/PencilSpinner.vue';
 
 const authStore = useAuth()
 
-const username = ref("")
-const password = ref("")
-const isSubmitting = ref(false)
-const errors = ref([])
+const state = reactive({
+    username: "",
+    password: "",
+    isSubmitting: false,
+    isLoading: true,
+})
 
 const route = useRoute()
 const code = route.query["code"]
-const isLoading = ref(true)
 
 // redirect to home if already logged in
 if (!code && !authStore.hasExpired) {
@@ -24,15 +25,15 @@ if (!code && !authStore.hasExpired) {
 async function onSubmit(event: Event) {
     event.preventDefault()
 
-    isSubmitting.value = true
-    await authStore.login(username.value, password.value)
+    state.isSubmitting = true
+    await authStore.login(state.username, state.password)
         .then(() => {
             if (!authStore.hasExpired)
                 router.push('/');
         })
         .catch()
         .finally(() => {
-            isSubmitting.value = false
+            state.isSubmitting = false
         })
 }
 onMounted(() => {
@@ -41,39 +42,33 @@ onMounted(() => {
         authStore.getAuthFromCode(code).then((path: string) => {
             router.push(path)
         }).finally(() => {
-            isLoading.value = false
+            state.isLoading = false
         })
     } else {
-        isLoading.value = false
+        state.isLoading = false
     }
 })
 </script>
 
 <template>
     <div class="col-md-6 offset-md-3 mt-5">
-        <PencilSpinner v-if="isLoading" />
+        <PencilSpinner v-if="state.isLoading" />
         <h2>Login</h2>
         <form id="loginForm" @submit="onSubmit">
             <div class="mb-3">
                 <label class="form-label">Username</label>
-                <input v-model="username" :required="true">
+                <input v-model="state.username" :required="true">
             </div>
             <div class="mb-3">
                 <label class="form-label">Password</label>
-                <input v-model="password" type="password" :required="true" :minlength="3">
+                <input v-model="state.password" type="password" :required="true" :minlength="3">
             </div>
             <div class="mb-3">
-                <button class="btn btn-primary" type="submit" value="submit" :disabled="isSubmitting">
-                    <span v-show="isSubmitting" class="spinner-border spinner-border-sm me-1"></span>
+                <button class="btn btn-primary" type="submit" value="submit" :disabled="state.isSubmitting">
+                    <span v-show="state.isSubmitting" class="spinner-border spinner-border-sm me-1"></span>
                     Login
                 </button>
             </div>
-            <p v-if="errors.values.length">
-                <b>Please correct the following error(s):</b>
-            <ul>
-                <li v-for="error in errors">{{ error }}</li>
-            </ul>
-            </p>
         </form>
     </div>
 </template>
