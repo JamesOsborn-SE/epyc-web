49120edbea99a7c458ce0e895a86087ee7acbe5b
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-09-03 12:12:46 -0700

    remove unused imports

--- frontend/src/views/OldGamesView.vue
+++ frontend/src/views/OldGamesView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from "vue"
+import { onMounted, ref } from "vue"
 import router from '@/router'
 import { useGame } from '@/stores/game';
 import type { AxiosError } from "axios";
